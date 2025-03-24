cdce38a240992b505d4da637a2d43ad080cf1857
Constantine <olexiy2004@gmail.com>
2025-03-24 10:20:31 +0100

    Connection of MongoDB, basic running server

---
+++ BackEnd/models/product.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true // createdAt, updatedAt
+});
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
